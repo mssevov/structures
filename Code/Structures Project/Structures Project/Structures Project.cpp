040ae32578f9afd4b06848d4ecaeee84a41ee921
Martin4ata <ms.sevov@gmail.com>
2020-04-13 12:46:06 +0300

    added menu to register a flight`

--- Code/Structures Project/Structures Project/Structures Project.cpp
+++ Code/Structures Project/Structures Project/Structures Project.cpp
@@ -22,28 +22,6 @@ struct CLIENT {
     struct BOARD_PASS bp;
 };
 
-enum MONTHS {
-    January = 1,
-    February = 2,
-    March = 3,
-    April = 4,
-    May = 5,
-    June = 6,
-    July = 7,
-    August = 8,
-    September = 9,
-    October = 10,
-    November = 11,
-    December = 12
-};
-
-struct TAKE_OFF_TIME {
-    int year;
-    enum MONTHS month;
-    int hour;
-    int minutes;
-};
-
 struct PLANES {
     string manufacturer;
     string modelName;
@@ -53,40 +31,42 @@ struct PLANES {
 struct FLIGHT {
     char flightNum[6];
     string origin, destination;
-    int distanceToDestination;
-    struct TAKE_OFF_TIME date;
+    float distanceToDestination;
     struct PLANES plane;
 };
 
 // Functions
-void mainMenu(bool &inMainLoop);
+void mainMenu(bool& inMainLoop, FLIGHT flight[], int& flightCounter);
+
+void registerFlight(FLIGHT flight[], int& flightCounter);
 
 // Main
 int main()
 {
 
     struct CLIENT client[20];
+    int flightCounter = 0;
     struct FLIGHT flight[20];
     bool inMainLoop = true;
     while (inMainLoop)
     {
-        mainMenu(inMainLoop);
+        mainMenu(inMainLoop, flight, flightCounter);
     }
 }
 
 // Functions
-void mainMenu(bool &inMainLoop)
+void mainMenu(bool& inMainLoop, FLIGHT flight[], int& flightCounter)
 {
     system("CLS");
     cout << "===========| Main Menu |===========\n";
-    
+
     cout << "\n0. Quit application...\n";
 
     cout << "\n-----/ Planes \\-----\n";
     cout << "1. Register a new Plane...\n";
     cout << "2. See Plane information...\n";
     cout << "3. Remove a Plane...\n";
-    
+
     cout << "\n-----/ Flight \\-----\n";
     cout << "4. Register a new Flight...\n";
     cout << "5. See Flight information...\n";
@@ -103,31 +83,22 @@ void mainMenu(bool &inMainLoop)
     case 48: //0 -> quit app
         inMainLoop = false;
         break;
-    case 49: //1 -> register a new plane
+    case 49: //1 -> register a new flight
+        registerFlight(flight, flightCounter); //function
+        break;
+    case 50: //2 -> see flight information
         //function
         break;
-    case 50: //2 -> see plane information
+    case 51: //3 -> remove a flight
         //function
         break;
-    case 51: //3 -> remove a plane
+    case 52: //4 -> register a new client
         //function
         break;
-    case 52: //4 -> register a new flight
+    case 53: //5 -> see client information
         //function
         break;
-    case 53: //5 -> see flight information
-        //function
-        break;
-    case 54: //6 -> remove a flight
-        //function
-        break;
-    case 55: //4 -> register a new client
-        //function
-        break;
-    case 56: //5 -> see client information
-        //function
-        break;
-    case 57: //6 -> remove a client
+    case 54: //6 -> remove a client
         //function
         break;
     default:
@@ -135,4 +106,20 @@ void mainMenu(bool &inMainLoop)
         break;
     }
 
+}
+
+void registerFlight(FLIGHT flight[], int& flightCounter)
+{
+    system("CLS");
+
+    cout << "Flight Number(6 symbols): "; cin >> flight[flightCounter].flightNum;
+    cout << "Country/City of origin: "; cin >> flight[flightCounter].origin;
+    cout << "Destination: "; cin >> flight[flightCounter].destination;
+    cout << "Travel Distance(km): "; cin >> flight[flightCounter].distanceToDestination;
+    cout << "Plane: ";
+    cout << "\n----Manufacturer: "; cin >> flight[flightCounter].plane.manufacturer;
+    cout << "----Model name: "; cin >> flight[flightCounter].plane.modelName;
+    cout << "----Max seats: "; cin >> flight[flightCounter].plane.maxSeats;
+
+    flightCounter++;
 }
